fa7782af907a56e27547e2b53accb6f1325ced61
TERMUX-KING-ALIF <138654302+TERMUX-KING-ALIF@users.noreply.github.com>
2024-06-13 11:47:53 +0600

    Update 00admin.js

--- modules/commands/00admin.js
+++ modules/commands/00admin.js
@@ -28,7 +28,7 @@ api.setMessageReaction("🙊", event.messageID, (err) => {}, true)
   //reply
   var msg = {
     body: `┼🩵🪽┼　　　　　　　　　　　　　　　　　　　　　┼🪶┼__•𝐀𝐥𝐡𝐚𝐦𝐝𝐮𝐥𝐢𝐥𝐥𝐚𝐡 𝐈 𝐚𝐦 𝐬𝐭𝐫𝐨𝐧𝐠 𝐛𝐞𝐜𝐚𝐮𝐬𝐞
-𝐈 𝐃𝐞𝐩𝐞𝐧𝐝 𝐨𝐟 𝐀𝐥𝐥𝐚𝐡......!!🌻🖤✨　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　┼🩵🪽─┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : 「『†« 𓆩𝗥𝗢𝗠𝗜𝗠𓆪 »†』 🩷 🪽」　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　~    𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : 「 𓆩 রমিম 𓆪 」　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　~　𝐖𝐏 𝐍𝐮𝐦𝐛𝐞𝐫 : 𓆩 m.me/01822583558 𓆪　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　　~　𝐎𝐭𝐡𝐞𝐫𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 : 𓆩 বলার সময় নাই 𓆪　　　　　 　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　　　~    𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 : 𓆩 কেউ প্রম করলে ইনবক্স 𓆪　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　┼🩵🪽─┼「𝗥𝟰𝗠!𝗠」┼🩵🪽─┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　┼「𝗥𝟰𝗠!𝗠」┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　 「 𓆩 𝐁𝐨𝐭 𓆪 」「  𓆩 Developer 𓆪  」        　　　　　　　　　　　　　　　　　　　   　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　   　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　༊𝐈𝐧𝐧𝐨𝐜𝐞𝐧𝐭﹏ლ『« 𓆩「𝗥𝗢𝗠𝗜𝗠」𓆪 »』 ﹏ღ`
+𝐈 𝐃𝐞𝐩𝐞𝐧𝐝 𝐨𝐟 𝐀𝐥𝐥𝐚𝐡......!!🌻🖤✨　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　┼🩵🪽─┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 : 「『†« 𓆩𝐁𝐎𝐒𝐒 𝐀𝐋𝐈𝐅𓆪 »†』 🩷 🪽」　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　~    𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧 : 「 𓆩আলিফ 𓆪 」　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　~　𝐖𝐏 𝐍𝐮𝐦𝐛𝐞𝐫 : 𓆩 m.me/DIMU NAH 𓆪　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　　~　𝐎𝐭𝐡𝐞𝐫𝐬 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 : 𓆩 বলার সময় নাই 𓆪　　　　　 　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　　　~    𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 : 𓆩 কেউ প্রম করলে ইনবক্স 𓆪　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　┼🩵🪽─┼「𝐵𐐵𓠵𔠵𔠠𝐀𝐋𝐈𝐅」┼🩵🪽─┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　　　　　　　　　　　　　　　　　　　　　　　　　　　~　　┼「𝐁𝐎𝐒𝐒 𝐀𝐋𝐈𝐅」┼　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　 「 𓆩 𝐁𝐨𝐭 𓆪 」「  𓆩 Developer 𓆪  」        　　　　　　　　　　　　　　　　　　　   　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　   　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　༊𝐈𝐧𝐧𝐨𝐜𝐞𝐧𝐭﹏ლ『« 𓆩「𝐵𐐵𓠵𔠵𔠠𝐀𝐋𝐈𝐅」𓆪 »』 ﹏ღ`
   }
   // Gọi bot
   var arr = ["এডমিন"];
@@ -61,4 +61,4 @@ module.exports.run = async function ({ api, event, Threads, getText }) {
   await Threads.setData(threadID, { data });
   global.data.threadData.set(threadID, data);
   return api.sendMessage(`${(data["এডমিন"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
-}
+} 
